2d4e1babebd1c28e134a4b8794e5b30532f8e2fc
officialmuneebahmad <officialmuneebahmad@gmail.com>
2025-04-22 19:56:27 +0500

    lmm-v3-008-Laptops Added + Model Added + Model Search Added + Bugs Fixed

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
     let currentPage = 1;
     const itemsPerPage = 9;
     const laptopList = document.getElementById("laptop-list");
-    const inputs = ['search', 'company', 'price', 'processor', 'generation', 'ram', 'storage', 'screen', 'graphics'];
+    const inputs = ['search', 'company', 'price', 'processor', 'generation', 'model', 'ram', 'storage', 'screen', 'graphics'];
     const filters = {};
     inputs.forEach(id => filters[id] = document.getElementById(id));
   
@@ -28,16 +28,17 @@ document.addEventListener("DOMContentLoaded", () => {
           <h2 class="text-lg font-bold">${laptop.name}</h2>
           <p class="text-sm">${laptop.company}</p>
           <p class="text-primary font-semibold">Rs.${laptop.price}/-</p>
-          <div class="flex flex-row flex-nowrap justify-between items-end gap-2">
+          <div class="flex flex-col flex-nowrap justify-between items-start gap-2">
             <ul class="text-sm space-y-1 mt-4">
             <li><strong>Processor:</strong> ${laptop.processor}</li>
             <li><strong>Generation:</strong> ${laptop.generation}</li>
+            <li><strong>Model:</strong> ${laptop.model}</li>
             <li><strong>RAM:</strong> ${laptop.ram}</li>
             <li><strong>Storage:</strong> ${laptop.storage}</li>
             <li><strong>Screen:</strong> ${laptop.screen}</li>
             <li><strong>Graphics:</strong> ${laptop.graphics}</li>
             </ul>
-              <a href="https://wa.link/laptopmartmultan" target="_blank" class="group text-[#252054] bg-[#fafafa] flex flex-row flex-nowrap items-center gap-2 px-4 py-2 bg-primary rounded-lg hover:bg-blue-600 hover:text-[#fafafa] transition">
+              <a href="https://wa.link/laptopmartmultan" target="_blank" class="w-full group text-[#252054] bg-[#fafafa] flex flex-row flex-nowrap justify-center items-center gap-2 px-4 py-2 bg-primary rounded-lg hover:bg-blue-600 hover:text-[#fafafa] transition">
               <svg class="group-hover:fill-[#fafafa]" xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#252054"><path d="M480-440v-360q0-17 11.5-28.5T520-840h280q17 0 28.5 11.5T840-800v200q0 17-11.5 28.5T800-560H600L480-440Zm80-200h200v-120H560v120Zm0 0v-120 120Zm238 520q-125 0-247-54.5T329-329Q229-429 174.5-551T120-798q0-18 12-30t30-12h162q14 0 25 9.5t13 22.5l26 140q2 16-1 27t-11 19l-97 98q20 37 47.5 71.5T387-386q31 31 65 57.5t72 48.5l94-94q9-9 23.5-13.5T670-390l138 28q14 4 23 14.5t9 23.5v162q0 18-12 30t-30 12ZM241-600l66-66-17-94h-89q5 41 14 81t26 79Zm358 358q39 17 79.5 27t81.5 13v-88l-94-19-67 67ZM241-600Zm358 358Z"/></svg>
               Call Now
               </a>
@@ -78,6 +79,7 @@ document.addEventListener("DOMContentLoaded", () => {
           (!filters.price.value || laptop.price <= parseFloat(filters.price.value)) &&
           (!filters.processor.value || laptop.processor.toLowerCase().includes(filters.processor.value.toLowerCase())) &&
           (!filters.generation.value || laptop.generation.toLowerCase().includes(filters.generation.value.toLowerCase())) &&
+          (!filters.model.value || laptop.model.toLowerCase().includes(filters.model.value.toLowerCase())) &&
           (!filters.ram.value || laptop.ram.toLowerCase().includes(filters.ram.value.toLowerCase())) &&
           (!filters.storage.value || laptop.storage.toLowerCase().includes(filters.storage.value.toLowerCase())) &&
           (!filters.screen.value || laptop.screen.toLowerCase().includes(filters.screen.value.toLowerCase())) &&
